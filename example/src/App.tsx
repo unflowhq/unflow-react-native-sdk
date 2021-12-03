e9fbaccb8d656553d8b5c89a6249d9d6f187cac2
Kavi Dhokia <kavi@spectrl.com>
2021-12-03 16:44:22 +0000

    fix view rendering

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-// import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import Unflow, { OpenerView } from 'unflow-react-native-sdk';
 
 export default function App() {
@@ -15,24 +15,30 @@ export default function App() {
   }, []);
 
   return (
-    // <View style={styles.container}>
-    // {/* //   <Text>Result: {result}</Text> */}
-    //   <OpenerView style={{ width: "100%", height: "100%" }}/>
-    // </View>
-    <OpenerView style={{ width: '100%' }} />
+    <SafeAreaView style={styles.container}>
+      <Text>Padding</Text>
+      <Text>Padding</Text>
+      <Text>Padding</Text>
+      <Text>Padding</Text>
+      <Text>Padding</Text>
+
+      <OpenerView
+        style={{ height: 80, width: '100%', backgroundColor: 'red' }}
+      />
+    </SafeAreaView>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     backgroundColor: 'green',
-//   },
-//   box: {
-//     width: 60,
-//     height: 60,
-//     marginVertical: 20,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'green',
+  },
+  // box: {
+  //   width: 60,
+  //   height: 60,
+  //   marginVertical: 20,
+  // },
+});
--- ios/UnflowReactNativeSdkViewManager.swift
+++ ios/UnflowReactNativeSdkViewManager.swift
@@ -4,17 +4,7 @@ import UnflowUI
 class UnflowReactNativeSdkViewManager: RCTViewManager {
     
     override func view() -> UIView! {
-        //        let label = UILabel()
-        //        label.text = "Swift Counter"
-        //        label.textAlignment = .center
-        //        return label
-        
-        let view = UIView()
-        let unflowViewController = OpenerViewController()
-        view.addSubview(unflowViewController.view)
-        return view
-        
-//        return URNOpenerView()
+        return URNOpenerView()
     }
     
     override static func requiresMainQueueSetup() -> Bool {
@@ -22,28 +12,52 @@ class UnflowReactNativeSdkViewManager: RCTViewManager {
     }
 }
 
-//@objc(URNOpenerView)
-//class URNOpenerView : UIView {
-//    var openerView : UIView? = nil
-//
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//
-//        let openerViewController = OpenerViewController()
-//        guard let openerView = openerViewController.view else { return }
-//
-//        openerView.frame = self.bounds
-//        openerView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-//        addSubview(openerView)
-//        self.openerView = openerView
-//    }
-//
-//    override func layoutSubviews() {
-//        super.layoutSubviews()
-//        self.openerView?.frame = self.bounds
-//    }
-//
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//}
+@objc(URNOpenerView)
+class URNOpenerView : UIView {
+    
+    weak var openerViewController: OpenerViewController?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if openerViewController == nil {
+            embed()
+        } else {
+            openerViewController?.view.frame = bounds
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func embed() {
+        guard let parentVC = parentViewController else {
+            return
+        }
+
+        let openerViewController = OpenerViewController()
+        parentVC.addChild(openerViewController)
+        addSubview(openerViewController.view)
+        openerViewController.view.frame = bounds
+        openerViewController.didMove(toParent: parentVC)
+        self.openerViewController = openerViewController
+    }
+}
+
+extension UIView {
+    var parentViewController: UIViewController? {
+        var parentResponder: UIResponder? = self
+        while parentResponder != nil {
+            parentResponder = parentResponder!.next
+            if let viewController = parentResponder as? UIViewController {
+                return viewController
+            }
+        }
+        return nil
+    }
+}
