412e406df5f75d5f946c6b32e5c78580836ae37e
Kavi Dhokia <kavi@spectrl.com>
2022-01-14 15:55:24 +0000

    tweak maps

--- android/src/main/java/com/unflow/reactnative/UnflowModule.kt
+++ android/src/main/java/com/unflow/reactnative/UnflowModule.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import com.facebook.react.bridge.*
 import com.unflow.androidsdk.UnflowSdk
 import com.unflow.androidsdk.ui.theme.Fonts
+import kotlin.reflect.KClass
+import kotlin.reflect.cast
 
 class UnflowModule(
   private val reactContext: ReactApplicationContext,
@@ -33,11 +35,13 @@ class UnflowModule(
       UnflowSdk.client().setUserId(userId = userId)
     }
 
+    @Suppress("UNCHECKED_CAST")
     @ReactMethod
     fun setAttributes(attributes: ReadableMap) {
-      val mappedAttributes = attributes.toHashMap() as? Map<String, String>
-      if (mappedAttributes != null) {
-        UnflowSdk.client().setAttributes(attributes = mappedAttributes)
+      val attributesMap = attributes.toHashMap() as? Map<String, Any>
+      val stringAttributes = attributesMap?.filterMapValueType(String::class)
+      if (stringAttributes != null) {
+        UnflowSdk.client().setAttributes(attributes = stringAttributes)
       }
     }
 
@@ -60,12 +64,20 @@ class UnflowModule(
 
     @ReactMethod
     fun trackEvent(eventName: String, metadata: ReadableMap) {
-      val mappedMetadata = metadata.toHashMap() as Map<String, Any?>
-      UnflowSdk.client().trackEvent(eventName, mappedMetadata)
+      val mappedMetadata = metadata.toHashMap() as? Map<String, Any?>
+      if (mappedMetadata != null) {
+        UnflowSdk.client().trackEvent(eventName, mappedMetadata)
+      } else {
+        UnflowSdk.client().trackEvent(eventName)
+      }
     }
 
     private fun ReadableMap.getFontResId(key: String): Int? {
       if (!hasKey(key)) return null
       return reactContext.resources.getIdentifier(getString(key), "font", reactContext.packageName)
     }
+}
+
+private inline fun <K, V, reified T : Any> Map<K, V>.filterMapValueType(type: KClass<T>): Map<K, T> {
+  return filterValues { type.isInstance(it) }.mapValues { type.cast(it) }
 }
