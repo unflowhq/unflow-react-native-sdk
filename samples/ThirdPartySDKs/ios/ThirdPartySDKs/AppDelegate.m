7b9afc9fc9849ccdda88259f22604107ac4eea5e
daibhin <david@monaru.io>
2022-01-26 14:50:19 +0000

    Add CodePush

--- samples/ThirdPartySDKs/ios/ThirdPartySDKs/AppDelegate.m
+++ samples/ThirdPartySDKs/ios/ThirdPartySDKs/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <CodePush/CodePush.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -55,7 +56,7 @@ static void InitializeFlipper(UIApplication *application) {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
 #endif
 }
 
