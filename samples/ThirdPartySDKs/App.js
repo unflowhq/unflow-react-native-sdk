612cabfd8ae4681b65b5765a0d32d8ccfce59798
David Newell <david@unflow.com>
2022-08-03 18:46:20 +0100

    Class component

--- samples/ThirdPartySDKs/App.js
+++ samples/ThirdPartySDKs/App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {Component, useEffect} from 'react';
 import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
@@ -31,29 +31,61 @@ function SettingsScreen() {
   );
 }
 
-const App = () => {
-  Unflow.initialize('50bcf564f0481f937f599a6b4567bc7f', true);
-  Unflow.setUserId('unflow-mirror_tabs-sample_14');
-  Unflow.sync();
+class App extends Component {
+  initializeUnflow = () => {
+    Unflow.initialize('ae469c05ed6d8f7c1d46d12901f661c9', true);
+    let uid = 'unflow-mirror_tabs-sample_3rd_6';
+    console.log(uid);
+    Unflow.setUserId(uid);
+    Unflow.sync();
+  };
 
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Home2" component={HomeScreen} />
-        <Tab.Screen
-          name="Settings"
-          component={SettingsScreen}
-          listeners={() => ({
-            tabPress: e => {
-              console.log('Visited Settings');
-              Unflow.trackEvent('Transitioned_Settings', {route: 'Settings'});
-            },
-          })}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-};
+  componentDidMount = async () => {
+    console.log('Hello');
+    this.initializeUnflow();
+    Unflow.trackEvent('AppStarted', {});
+    Unflow.sync();
+  };
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Explore" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
+// const App = () => {
+//   useEffect(() => {
+//     console.log('Hiya');
+//     Unflow.initialize('50bcf564f0481f937f599a6b4567bc7f', true);
+//     Unflow.setUserId('unflow-mirror_tabs-sample_13');
+//     Unflow.sync();
+//   }, []);
+
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator>
+//         <Tab.Screen name="Home" component={HomeScreen} />
+//         <Tab.Screen name="Home2" component={HomeScreen} />
+//         <Tab.Screen
+//           name="Settings"
+//           component={SettingsScreen}
+//           listeners={() => ({
+//             tabPress: e => {
+//               console.log('Visited Settings');
+//               Unflow.trackEvent('Transitioned_Settings', {route: 'Settings'});
+//             },
+//           })}
+//         />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// };
 
 export default App;
